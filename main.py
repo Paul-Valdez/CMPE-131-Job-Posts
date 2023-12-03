8d7210a47fabe347445cb6c911190d4075f11aee
Pikalot <143025777+Pikalot@users.noreply.github.com>
2023-12-03 09:41:55 +0000

    test new gotrue

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 from supabase import create_client
 import gotrue
 from gotrue.errors import AuthApiError
+import traceback
 load_dotenv()
 
 supabase_url = os.getenv('SUPABASE_URL')
@@ -273,10 +274,11 @@ def applied_success():
 
 @app.errorhandler(gotrue.errors.AuthApiError)
 def handleError(e):
-  print(e.message, flush=True)
-  if(e.message == "Invalid login credentials"):
-    return redirect(url_for("login", invalidCredentials=True))
-  return "Error"
+    print(e.message, flush=True)
+    traceback.print_exc()
+    if e.message == "Invalid login credentials":
+        return redirect(url_for("login", invalidCredentials=True))
+    return "Error"
 
 # script entry point
 if __name__ == "__main__":
